9f268dee1a2fdabafefc81af39645dc71972bf8f
kylemurray2 <kylemurray2@gmail.com>
2017-07-17 10:26:52 -0400

    Fixed some bugs. This script may not be necessary.

--- mask_unwrapped.m
+++ mask_unwrapped.m
@@ -1,6 +1,6 @@
 %Mask Unwrapped
 clear all;close all
-gammathresh=.2;
+gammathresh=.4;
 getstuff
 
 %load mask file
@@ -9,28 +9,33 @@ mask_file=maskfilerlk{1};
 fid=fopen(mask_file,'r','native');
 msk=fread(fid,[newnx,newny],'real*4');
 fclose(fid);
-% msk=fliplr(msk');
 figure;imagesc(msk);colorbar
 
-for ii=1:nints
+parfor ii=1%:nints
   filename=[ints(ii).unwrlk{1}];
   display(['Masking ' ints(ii).name])
   fid         = fopen(filename,'r','native');
-  [rmg,count] = fread(fid,[newnx,newny*2],'real*4');
-  status      = fclose(fid);
-  phs         = (rmg(1:newnx,2:2:newny*2));
-%   phs=fliplr(phs');
+  [phs,count] = fread(fid,[newnx,newny],'real*4');
+  fclose(fid);
    fid=fopen('phs_unmasked','w','native');
    fwrite(fid,phs,'real*4')
    fclose(fid)
-%   figure;imagesc(phs);colorbar
   
-  phs(msk<gammathresh)=0;
+  phs(find(msk<gammathresh))=0;
   
   fid=fopen('phs_masked','w','native');
-     fwrite(fid,phs,'real*4')
+     fwrite(fid,phs,'real*4');
      fclose(fid);
 
    system(['mag_phs2rmg phs_unmasked phs_masked ' ints(ii).unwrlk{1} ' ' num2str(newnx)]);
   
 end
+
+
+% Split back to r4
+% clear all;close all
+% getstuff
+% for ii=1%:nints
+%     movefile(ints(ii).unwrlk{1},[ints(ii).unwrlk{1} '_o']); 
+%    system(['rmg2mag_phs ' ints(ii).unwrlk{1} '_o ' ints(ii).unwrlk{1} ' jnk ' num2str(2840)]);
+% end
