49e4006ed6f97bdc7ca41f5baa6d480f1b8d9133
kylemurray2 <kylemurray2@gmail.com>
2017-07-17 10:26:52 -0400

    Some file checking utilities added.

--- unwrap_flat.m
+++ unwrap_flat.m
@@ -1,27 +1,52 @@
-set_params
-load(ts_paramfile);
+getstuff
+l=1;k=1;
 
-ndates  = length(dates);
-nints   = length(ints);
-if strcmp(sat,'S1A')
-    nx=ints(id).width;
-    ny=ints(id).length;
+
+if(exist([ints(k).unwrlk{l} '_flat'],'file'))
+    reply = input('*.unw_flat already exist.  Want to use them to make new .unw files? ', 's')
+    if(isempty(reply))
+        reply='Y';
+    end
+    switch reply
+        case {'Y','yes','Yes','y','YES','yep'}
+            disp('Overwriting *unw files, using *unw_flat.  Using tiles')
+            flag=1;
+        case {'No','no','N','n','NO','nope'}
+            disp('Doing nothing..')
+            flag=0;
+            return
+    end
 else
-    [nx,ny]     = load_rscs(dates(id).slc,'WIDTH','FILE_LENGTH');
-
+    disp('*.unw_flat do not exist yet. Tagging .unw with _flat, and reunwrapping the _flat files.')
+    flag=1;
 end
-newnx=floor(nx./rlooks)
-newny=floor(ny./alooks);
 
-for l=1:length(rlooks)
-    for k=1:nints
-        if(~exist(ints(k).unwrlk{l},'file'))
-%             movefile(ints(k).unwrlk{l},[ints(k).unwrlk{l} '_flat']); %save original version
-            command=['snaphu -s ' ints(k).unwrlk{l} '_flat -u ' num2str(newnx(l)) ' -o ' ints(k).unwrlk{l} ' --tile 4 4 150 150'];
+
+if(flag==1)
+    for l=1:length(rlooks)
+        for k=1%:nints
+            if(~exist([ints(k).unwrlk{l} '_flat'],'file'))
+                movefile([ints(k).unwrlk{l}],[ints(k).unwrlk{l} '_flat']); %save original version
+            end
+            command=['snaphu -s ' ints(k).unwrlk{l} '_flat -u ' num2str(newnx(l)) ' -o ' ints(k).unwrlk{l} ' -c ' rlkdir{l} 'mask.cor --mcf --tile 7 7 150 150'];
             mysys(command);
+            
         end
     end
 end
 
 
+% l=1
+%     for k=1:nints
+%                  if(exist([ints(k).unwrlk{l} '_flat'],'file'))
+%
+%              movefile([ints(k).unwrlk{l} '_flat'],[ints(k).unwrlk{l}]); %save original version
+%                  end
+%     end
 
+
+
+
+
+%command=['snaphu -s ' ints(k).unwrlk{l} '_flat -u ' num2str(newnx(l)) ' -o ' ints(k).unwrlk{l} ' -c ' rlkdir{l} 'mask.cor --tile 7 7 150 150'];
+%
