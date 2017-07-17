214e81757bbf051010a9c4fd22b6cb0afc438b82
kylemurray2 <kylemurray2@gmail.com>
2017-07-17 10:26:52 -0400

    Modified for parallel processing and to take .r4 files instead of .c8.

--- unwrap_rlooks.m
+++ unwrap_rlooks.m
@@ -1,46 +1,18 @@
-set_params
-load(ts_paramfile);
+% Unwrap_rlooks
+%<^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^
+%   Kyle Murray; July 2017
+%   Takes *bell ints and outputs r4 .unw files.  Uses 35 core parallel
+%   processing and 20x14 tiles
+%<^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^><^
 
-ndates  = length(dates);
-nints   = length(ints);
+clear all;close all
+getstuff
 
-if strcmp(sat,'S1A')
-    nx=ints(id).width;
-    ny=ints(id).length;
-else
-    [nx,ny]     = load_rscs(dates(id).slc,'WIDTH','FILE_LENGTH');
+write_snaphu_conf; %uses parallel processing and tiles
 
-end
-     newnx   = floor(nx./rlooks)
-     newny   = floor(ny./alooks);
-     
-% %mask using mask.cor
-% fid=fopen([rlkdir{1} 'flat_' ints(id).name '_8rlks.rect.cor' ],'r');
-% msk=fread(fid,[newnx,newny*2],'real*4');
-% 
-% msk=msk';
-% msk=msk(1:2:newny,:);
-% fclose(fid)
-% 
-% for l=1:length(rlooks)
-%         for i=1:1
-%             fid=fopen([ints(i).flatrlk{l}],'r');
-%             tmp         = fread(fid,[newnx,newny],'real*4');
-%             tmp(find(msk<.5))=0;
-%             fidout=fopen([ints(i).flatrlk{l} '_new'],'w','native');
-%             fwrite(fidout,tmp,'real*4')
-%         end
-% end
-
-
-
-for l=1:length(rlooks)
-    for k=1:nints     
-        if(~exist([ints(k).unwrlk{1} '_b'],'file'))
-            command=['snaphu -s ' ints(k).flatrlk{1} '_bell ' num2str(newnx(l)) ' -o ' ints(k).unwrlk{1} ' -c ' rlkdir{l} 'mask.cor  --tile 5 5 150 150'];
-            mysys(command);
-        end
+    for k=1:nints
+        %         if(~exist([ints(k).unwrlk{1}],'file'))
+        disp(['unwrapping ' ints(k).flatrlk{1}]);
+        system(['snaphu -f ' ints(k).unwrlk{1} '_snaphu.conf >> tmp_log']);
+        %         end
     end
-end
-
-!rm -r TS/looks4/snaphu*
