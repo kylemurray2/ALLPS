fc021e1781a50a6b0daf25160b56e471b5133125
kylemurray2 <kylemurray2@gmail.com>
2017-09-14 14:45:58 -0400

    maybe we should generalize the threshold values and filter size?

--- ps_interp.m
+++ ps_interp.m
@@ -3,25 +3,25 @@
 clear all;close all
 set_params
 load(ts_paramfile)
+
 %Define these things:______________________________________________________
 filtmode=1; %1 for bell filter, 2 for cone filter
-gamma=0.7; %threshold for gamma0 mask (high values for gamma and alpha are good)
-alpha=0.9; %threshold for correlation in individual int
-rx=4; ry=rx*pixel_ratio; %bell radius (used in filtmode 1)
-R=2; %cone radius (used in filtmode 2)
+gamma=0.4; %threshold for gamma0 mask (high values for gamma and alpha are good)
+alpha=0.3; %threshold for correlation in individual int
+ry=5; rx=ry; %bell radius (used in filtmode 1)
 %__________________________________________________________________________
 
 im     = sqrt(-1);
-system(['mkdir ' rlkdir{1} 'orig']);
+system(['mkdir ' rlkdir 'orig']);
 %load mask.cor
-mskfile=[rlkdir{1} 'mask_4rlks.r4'];
-fid=fopen( maskfilerlk{1},'r','native');
+mskfile=[rlkdir 'mask_4rlks.r4'];
+fid=fopen( maskfilerlk,'r','native');
 msk=fread(fid,[newnx,newny],'real*4');
 % msk=flipud(msk');
 fclose(fid);
+figure;imagesc(msk);colorbar
 
-
-parfor ii=13%:nints
+parfor ii=1:nints
 %     if(~exist([ints(ii).flatrlk{1} '_bell']))
         par_ps_interp(ii,msk,gamma,alpha,rx,ry)
 %     end
