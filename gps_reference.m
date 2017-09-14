6b5a3a409622a7bea1eab79d1481fa6323c2c143
kylemurray2 <kylemurray2@gmail.com>
2017-09-14 15:20:46 -0400

    mostly comments

--- gps_reference.m
+++ gps_reference.m
@@ -1,44 +1,67 @@
+
 % GPS InSAR referencing
 % Kyle Murray
 % July, 2017
 % Description:
-%   Loads all available UNR GPS time series in a given lat/lon range
-%   Find reference date in all GPS sites.  Displacement should be zero at this date in all GPS TS's 
+%   Loads all available UNR GPS time series in a given lat/lon range with
+%   get_gps_ts script. All time series are put into a directory GPS
+%   Finds reference date in all GPS sites.  Displacement should be zero at 
+%   this date in all GPS TS's 
 %   
 clear all; close all
 getstuff
 
-max_lat = max(frames.lat);
-min_lat = min(frames.lat);
-max_lon = max(frames.lon);
-min_lon = min(frames.lon);
+dl_files=0;
+%   dl_files=1:  Download new files
+%   dl_files=0:  Just load the velocities
+[enu_vels,llh_vels,sites_vels]=get_gps_ts_vels(dl_files); 
 
-!mkdir GPS
-chdir('GPS')
+% Plot horizontal
+figure;quiver(llh_vels(:,2),llh_vels(:,1),enu_vels(:,1),enu_vels(:,2))
+title('Horizontal GPS velocities');axis image; kylestyle
+xlabel('Longitude');ylabel('latitude')
 
-% Download GPS lat/lon file from UNR
-!rm llh*
-system('wget ftp://gneiss.nbmg.unr.edu/rapids/llh');
+% Plot Vertical
+figure;quiver(llh_vels(:,2),llh_vels(:,1),zeros(length(enu_vels(:,3)),1),enu_vels(:,3))
+title('Vertical GPS velocities');axis image; kylestyle
+xlabel('Longitude');ylabel('latitude');hold on
+text(llh_vels(:,2),llh_vels(:,1),sites_vels)
 
-% Get list of GPS site within area of interest
-fid= fopen('llh');
-sllh= textscan(fid,'%s%f%f%f','headerLines',1); %site lat lon height
-sites=sllh{1,1};
-lats =sllh{1,2};
-lons =sllh{1,3};
-heights =[sllh{1,4}];
-clear sllh
+% %off map
+offmap={'WGPP'; 'P782';'P602'; 'P578';'P571';'P567';'P546';'P540'
+'P539';'P522';'P518';'GDEC';'EDPP';'CUHS';'BCWR';'ARM2';'ARM1' };
 
-idx=(lats>min_lat & lats<max_lat & lons>min_lon & lons<max_lon);
-sites=sites(idx);
-llh=[lats(idx) lons(idx) heights(idx)]; %lat lon height
-clear lats lons heights idx
+% [~,~,bad_ids] =  intersect(offmap(:),sites_vels(:));
+% llh_vels(bad_ids,:)=[];
+% sites_vels(bad_ids)=[];
+% enu_vels(bad_ids,:)=[];
+% % Give it a vector of indices
+% site_idx=1:length(sites_vels);
+% plot_pts_geo(llh_vels(site_idx,1),llh_vels(site_idx,2),sites_vels(site_idx))
+
+% main subsidence lat lon
+a=[35.93, -119.4];
+% lost hills lat lon
+b=[35.43, -119.7];
+
+subsidence=[a;b];
 
 
-% Download GPS time series of all sites in list
-!rm *tenv3*
-for ii=1:length(sites)
-readGPS_TS(sites{ii}, 1);
-end
+% Just do a subset of the better GPS sites
+% good_subset={'P565';'P564';'P563';'P547';'P545';'P544';'P543';'P541';'P538'
+%     'P537';'P536';'P056';'CRCN';'BVPP';'BKR2';'BKR1';'BAK1'};
+good_subset={'CRCN'};
+[~,~,good_ids] =  intersect(good_subset(:),sites_vels(:));
+plot_all_pts(llh_vels(good_ids,1),llh_vels(good_ids,2),sites_vels(good_ids)) %plots insar and gps
+% plot_pts_geo(subsidence(:,1),subsidence(:,2)); %This also plots profiles
 
-% 
+
+%ideas:
+% use GPS in decorrelated areas, and interpolate points around it with good
+% insar dates
+
+% interpolate all GPS displacements to make a priori guesses for each
+% interferogram before unwrapping?  
+
+% find areas of low deformation
+
