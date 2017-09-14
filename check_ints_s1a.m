4ff82b7a70b61e08252545906005ceeb9061c3b7
kylemurray2 <kylemurray2@gmail.com>
2017-09-14 14:45:58 -0400

    variable changes

--- check_ints_s1a.m
+++ check_ints_s1a.m
@@ -1,12 +1,11 @@
 %checks the ints struct to make sure we have int dirs for all of them
+clear all;close all
 
 set_params
-load(ts_paramfile)
-
 badids=[];
 for i=1:nints
-if(~exist(ints(i).topophase))
-   disp([ints(i).topophase 'does not exist (number ' num2str(i) ')'])
+if(~exist(ints(i).flat))
+   disp([ints(i).flat 'does not exist (number ' num2str(i) ')'])
    badids= [badids;i];
 end
 end
@@ -20,12 +19,13 @@ if(exist('badids'))
     switch reply
         case {'Y','Yes','y','YES'}
             disp('Deleting missing pairs from structure!')
+            disp('Process more ints to bridge the gap.')
+
             ints([badids])=[];
         case {'No','n','N','NO'}
             disp('Process missing ints before continuing!')
             return
     end
-    
     save(ts_paramfile,'dates','ints')
 else
     disp('No missing pairs :)')
